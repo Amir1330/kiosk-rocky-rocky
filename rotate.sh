55211102446472eede8007e2483394030f694cb9
Amir <iskakovamir7486@gmail.com>
2025-08-28 22:16:16 +0600

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# rotate.sh – Rotate GNOME Wayland display easily
+# Rotate GNOME Wayland display automatically
 
-# Get first connected monitor name
+# Find first monitor name
 MONITOR=$(gdbus call --session \
   --dest org.gnome.Mutter.DisplayConfig \
   --object-path /org/gnome/Mutter/DisplayConfig \
   --method org.gnome.Mutter.DisplayConfig.GetResources \
   | grep -o '"connector":"[^"]*"' | head -n1 | cut -d':' -f2 | tr -d '"')
 
-# Pick transform value based on argument
+# Transform values
 case "$1" in
   normal)   TRANSFORM=0 ;;
   right)    TRANSFORM=1 ;;
@@ -17,9 +17,9 @@ case "$1" in
   *) echo "Usage: $0 {normal|right|inverted|left}"; exit 1 ;;
 esac
 
-# Apply rotation
+# Apply rotation (note the array + variant syntax!)
 gdbus call --session \
   --dest org.gnome.Mutter.DisplayConfig \
   --object-path /org/gnome/Mutter/DisplayConfig \
   --method org.gnome.Mutter.DisplayConfig.ApplyMonitorsConfig \
-  1 "[{'logical_monitors':[{'x':0,'y':0,'scale':1,'primary':true,'transform':$TRANSFORM,'monitor_configs':[{'id':'$MONITOR'}]}]}]"
+  1 "[{'x':0,'y':0,'scale':1,'primary':true,'transform':$TRANSFORM,'monitor_configs':[{'connector':'$MONITOR'}]}]" "{}"
